f7b93433769d6d2d0bfcd1e5c030955912051314
Factory <mitar-c@hotmail.com>
2020-03-03 10:33:27 +0100

    jquery slider update

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
 $(document).ready(function(){
     $('#next').on('click', function () {
-        var last = $('.image1').last().css({opacity: '0', width: '0px'});
-        last.prependTo('.slideshow1');
-        last.animate({opacity: '2', width: $('.image1').last().width()});
+        var button = $(this);
+        button.attr('disabled', true);
+
+        var last = $('.image1').last();
+        var imageWidth = last.width();
+        last.css({width: '0px'}).prependTo('.slideshow1');
+        last.animate({width: imageWidth}, 400, function(){
+            button.removeAttr('disabled');
+        });
     });
 
     $('#next').on('click', function () {
-        var last = $('.image2').last().css({opacity: '0', width: '0px'});
-        last.prependTo('.slideshow2');
-        last.animate({opacity: '2', width: $('.image2').last().width()});
+        var last = $('.image2').last();
+        var imageWidth = last.width();
+        last.css({width: '0px'}).prependTo('.slideshow2');
+        last.animate({width: imageWidth}, 400);
     });
 
     $('#prev').on('click', function () {
+        var button = $(this);
+        button.attr('disabled', true);
+
         var first = $('.image1').first();
-        first.animate({opacity: '0', width: '0px'}, function() {
-            first.appendTo('.slideshow1').css({opacity: '1', width: $('.image1').first().width()});
+        var imageWidth = first.width();
+        first.animate({width: '0px'}, 400, function() {
+            first.appendTo('.slideshow1').css({width: imageWidth});
+            button.removeAttr('disabled');
         });
+
     });
 
     $('#prev').on('click', function () {
         var first = $('.image2').first();
-        first.animate({opacity: '0', width: '0px'}, function() {
-            first.appendTo('.slideshow2').css({opacity: '1', width: $('.image2').first().width()});
+        var imageWidth = first.width();
+        first.animate({width: '0px'},400, function() {
+            first.appendTo('.slideshow2').css({width: imageWidth});
         });
     });
     
